d75d771150b5dabde75455149960b8689ae0335f
Dhananjay _singh026 <92293458+Dhananjaysingh-026@users.noreply.github.com>
2022-05-30 17:23:34 +0000

    Add files via upload

---
+++ 3 num.py
@@ -0,0 +1,9 @@
+a = int(input('First number:'))
+b = int(input('Second number:'))
+c = int(input('Third number:'))
+if (a>b)and(a>c):
+ print("First number is large")
+elif (b>a)and(b>c):
+     print("Second number is large")
+else:
+    print("Third number is large")
